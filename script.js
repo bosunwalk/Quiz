9df3d7bfdf43e8ccac836a4cbc0ba827bd6f78da
olatunbosun olotu <olotubushido1@gmail.com>
2022-11-04 14:24:59 +0100

    Add files via upload
    Added an answer html page

--- script.js
+++ script.js
@@ -1,89 +1,181 @@
 const quizData = [
     { //   1
-        question: 'At the time of writing (March 2022) how many episodes of The Simpsons are there?',
-        a: "973",
-        b: "730",
-        c: "990",
-        d: "850",
+        question: ' #1. How many countries were participated as founding member of United Nation?',
+        a: "45",
+        b: "50",
+        c: "51",
+        d: "75",
         correct: "c",
     }, { // 2
-        question: 'What does He stand for on the periodic table?',
+        question: '#2. What does He stand for on the periodic table?',
         a: "Helium",
         b: "Helienium",
         c: "Heroin",
         d: "Hemisphere",
         correct: "a",
     },{ // 3
-        question: "What is the capital of finland?",
+        question: "#3. What is the capital of finland?",
         a: "Honolulu",
         b: "Helsinki",
         c: "Venice",
         d: "Luton",
         correct: "b", 
     },{ // 4
-        question: "Which five colours make up the Olympic rings?",
+        question: "#4. Which five colours make up the Olympic rings?",
         a: "Black, green, blue, yellow and red",
         b: "Black, cream, indido, yellow, crisom",
         c: "Black, lime, violet, orange, pink",
         d: "Black, green, violet, yet, maroon",
         correct: "a",
     },{ // 5
-        question: "In Harry Potter, what is the name of The Weasleys house?",
+        question: "#5. In Harry Potter, what is the name of The Weasleys house?",
         a: "The refuge",
         b: "The Homely",
         c: "The Haven",
         d: "The borrow",
         correct: "d",
     },{ // 6 
-        question:"To a single decimal point, how many kilometers are in a mile?",
+        question:"#6. To a single decimal point, how many kilometers are in a mile?",
         a:"1.6km",
         b:"1.4km",
         c:"1.9km",
         d:"1.5km",
         correct: "a",
     },{// 7
-        question:"Which planet is closest to the sun?",
+        question:"#7. Which planet is closest to the sun?",
         a:"Mercury",
         b:"Venus",
         c:"Neptune",
         d:"Pluto",
         correct: "a",
     },{ // 8
-        question:"What was the most streamed show on Netflix in 2020?",
+        question:"#8. What was the most streamed show on Netflix in 2020?",
         a:"The Umbrella Academy",
         b:"The Young Royals, with 43million stream",
         c:"The Sandman,",
         d:"Wu Assasins",
         correct: "a",
     },{ // 9
-        question:"Which one is the smallest ocean in the World? ",
+        question:"#9. Which one is the smallest ocean in the World? ",
         a:"Indian",
         b:"Pacific",
         c:"Atlantic",
         d:"Arctic",
         correct: "d",
     }, { //10
-        question:"Which country gifted the statute of liberty to USA in 1886?",
+        question:"#10. Which country gifted the statute of liberty to USA in 1886?",
         a:"Canada",
         b:"Brazil",
         c:"France",
         d:"England",
         correct: "c",
     },{ // 11
-        question:"Dead sea is located bettwen which two countries?",
+        question:"#11. Dead sea is located bettwen which two countries?",
         a:"Jordan and Sudan",
         b:"Jordan and Israel",
         c:"Turkey and UAE",
         d:"UAE and Egypt",
         correct: "b",
     },{ // 12
-        question:"Which ocean 'Bermuda Triangle' region is located?",
+        question:"#12. Which ocean 'Bermuda Triangle' region is located?",
         a:"Atlantic",
         b:"Indian",
         c:"Pacific",
         d:"Arctic",
         correct: "a",
-    },
+    },{ //13
+        question:"#13. Which country is known as the 'playground of Europe?",
+        a:"Austria",
+        b:"Holland",
+        c:"Switzerland",
+        d:"Italy",
+        correct: "c",
+    },{ // 14
+        question:"#14. Which country is also known as 'Landof the rising Sun'?",
+        a:"Japan",
+        b:"New Zealand",
+        c:"Fiji",
+        d:"China",
+        correct: "a",
+    },{ //15
+        question:"#15. Which country is known as the 'Land of Thunderbolts'? ",
+        a:"China",
+        b:"Bhutan",
+        c:"Mongolia",
+        d:"Thailand",
+        correct: "b",
+    },{ // 16
+        question:"#16. Which continent has the highest number of countries?",
+        a:"Asia",
+        b:"Europe",
+        c:"North America",
+        d:"Africa",
+        correct: "d",
+    },{ // 17
+        question:"#17. Mount Kilimanjaro is located in which country? ",
+        a:"Tanzania",
+        b:"Ghana",
+        c:"Namibia",
+        d:"South Africa",
+        correct: "a",
+    },{// 18
+        question:"#18. Blue Nile and White Nile meet at?",
+        a:"Khartoum",
+        b:"Cairo",
+        c:"Marrakesh",
+        d:"Lagos",
+        correct: "a",
+    },{//19
+        question:"#19. The river that crosses Equator two times is?",
+        a:"Congo",
+        b:"Nile",
+        c:"Limpopo",
+        d:"Niger",
+        correct: "a",
+    },{//20
+        question:"#20. Lake Victoria does not border?",
+        a:"Tanzania",
+        b:"Uganda",
+        c:"Kenya",
+        d:"Zimbabwe",
+        correct: "d",
+    },{ // 21
+        question:"#21. Which of the following African country is landlocked? ",
+        a:"South Africa",
+        b:"Ethiopia",
+        c:"Libya",
+        d:"Nigeria",
+        correct: "b",
+    },{// 22
+        question:"#22. Tropic of cancer does not pass through?",
+        a:"Mali",
+        b:"Mauritina",
+        c:"Algeria",
+        d:"Congo",
+        correct: "d",
+    },{ //23
+        question:"#23. Tropic of cancer does not pass through? ",
+        a:"Steppe",
+        b:"Pampas",
+        c:"Savanna",
+        d:"Downs",
+        correct: "c",
+    },{// 24
+        question:"#24.Which country does not fall in the horn of Africa?",
+        a:"Djibouti",
+        b:"Egypt",
+        c:"Ethiopa",
+        d:"Kenya",
+        correct:"d",
+    },{// 25
+        question:"#25. Which African country shares its boundary with Mediterranean Sea?",
+        a:"Libya",
+        b:"Kenya",
+        c:"Congo",
+        d:"Uganda",
+        correct:"a",
+
+    }
 
 
 ];
@@ -155,7 +247,10 @@ submitBtn.addEventListener("click", () =>{
             questions.</h2>
              
             <button onclick= "location.reload()
-            ">Reload</button>`;
+            ">Reload</button>
+             
+            <a href="/answers.html"><button
+            ">See Answers</button></a>`;
         }
     }
 });
